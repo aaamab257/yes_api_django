10157044a7c29a184a8103f2042599d93caa43e8
Ahmed Ashraf <ahmedashraf@Ahmeds-iMac-Pro.local>
2022-11-06 14:12:28 +0200

    edit some bugs

--- account/models.py
+++ account/models.py
@@ -58,7 +58,7 @@ class User(AbstractBaseUser , PermissionsMixin):
   state = models.CharField(max_length=255)
   school = models.CharField(max_length=255)
   phone_number = models.CharField(max_length=15)
-  is_active = models.BooleanField(default=False)
+  is_active = models.BooleanField(default=True)
   is_admin = models.BooleanField(default=False)
   is_superuser = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
